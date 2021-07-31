d4355a43711a71189c98519ccfcf6f36e79808e4
BrettAGobel <brettg841@gmail.com>
2021-07-31 14:47:21 -0600

    tryna set up the backend and frontend server to run concurrently

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3001;
+
+app.get('/api', (req, res) => {
+    res.json({message: "Hello from the server, are you ready to make a messaging appp?"})
+})
+
+app.listen(port, () => {
+    console.log(`Example apppppp listening at http://localhost:${port}`)
+})
